c28eb672a4a931a127234085ba688acd6bb43620
Юлия Волкова <volktul@gmail.com>
2022-11-06 17:32:20 +0300

    Добавьте файлы проекта.

---
+++ SR6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str2 = "";
+            str = str.Replace(" ", "");
+            str = str.ToLower();
+            foreach (char c in str)
+            {
+                str2 = c + str2;
+
+            }
+            if (str ==str2)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else Console.WriteLine("Строка не является палиндромом");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SR6_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '{')
+                {
+                    for (int j = i + 1; j < s.Length; j++)
+                    {
+                        if (s[j] == '}')
+                        {
+                            s = s.Substring(0, i) + s.Substring(j + 1);
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+        }
+    }
+}
